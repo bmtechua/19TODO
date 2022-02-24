87dd62d8a43c95ba63a3709338ae1ed68442a14d
bmtechua <bmtech@i.ua>
2022-02-24 15:28:16 +0200

    Initial Commit

---
+++ 19TODO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19TODO
+//
+//  Created by bmtech on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
