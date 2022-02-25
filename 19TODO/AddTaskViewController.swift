f5fbba6f73abfb3add0c1df4b5bf2d322cc36a31
bmtechua <bmtech@i.ua>
2022-02-25 18:07:59 +0200

    add button and textfields + textfield's properties

--- 19TODO/AddTaskViewController.swift
+++ 19TODO/AddTaskViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class AddTaskViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var nameTextView: UITextField!
+    @IBOutlet weak var descTextView: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameTextView.layer.borderWidth = 0.5
+        nameTextView.layer.cornerRadius = 5
+        nameTextView.layer.masksToBounds = false
+        nameTextView.layer.shadowRadius = 2
+        nameTextView.layer.shadowOpacity = 1.0
+        nameTextView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        nameTextView.layer.shadowColor = UIColor.gray.cgColor
+        
+        descTextView.layer.borderWidth = 0.5
+        descTextView.layer.cornerRadius = 5
+        descTextView.layer.masksToBounds = false
+        descTextView.layer.shadowRadius = 2
+        descTextView.layer.shadowOpacity = 1.0
+        descTextView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        descTextView.layer.shadowColor = UIColor.gray.cgColor
 
-        // Do any additional setup after loading the view.
+        
+        addButton.layer.cornerRadius = 16
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
+
